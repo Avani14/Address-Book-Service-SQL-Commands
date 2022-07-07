40c832ec936ecbace4cf6db88e4ae338c70b106f
Avani14 <trivediavani14@gmail.com>
2022-07-07 22:11:09 +0530

    Ability to create a table.

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -1,4 +1,14 @@
--- Database created
 create database Address_Book_Service;
 use Address_Book_Service;
 show databases;
+
+create table address_book(first_name varchar(50),
+	last_name varchar(50), 
+    address varchar(60), 
+    city varchar(20),
+	state varchar(20), 
+    zip int, 
+    phone_number long,
+    email varchar(50));
+
+select * from address_book;
